fb7aa398a5136746c0a204ad50aa99b4f72fd8a2
Akshay Chame <akshay.chame@tmdc.io>
2026-07-09 19:51:33 +0530

    Remove stray non-engine files from bundled vulcan-examples/
    Drops extra/, bigquery/, fabric/, mssql/, mysql/, redshift/ (not in
    ALLOWED_ENGINES) plus stray config.yaml, domain-resource.yaml,
    README.md, package-lock.json, requirements.txt, .gitignore, and
    non-README markdown files from the 5 supported engine folders.

--- dpbs-docs/vulcan-examples/extra/orders360/macros/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Project macros for the Orders360 Vulcan example.
-
-SQLMesh/Vulcan will auto-discover macros in this package.
-"""
-
-from .orders360 import *  # noqa: F401,F403
-
--- dpbs-docs/vulcan-examples/extra/snowflake/audits/validate_customer_id.sql
+++
@@ -1,7 +0,0 @@
-AUDIT (
-  name validate_customer_id
-);
-
-
-select * from 
-@this_model where CUSTOMER_ID is null
